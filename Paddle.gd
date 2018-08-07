d3a5b9dcd4c82e673db99f3b4840328d77979098
Joafie <jade424433@gmail.com>
2018-08-07 15:10:45 -0700

    Changed paddle to kinematicbody, centered score counter

--- Paddle.gd
+++ Paddle.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends RigidBody2D
 
 export (int) var speed
 signal hit
@@ -7,11 +7,11 @@ onready var rect_size = get_viewport_rect().size
 func _ready():
 	hide()
 
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_mouse_button_pressed(1):
 		position.x = get_viewport().get_mouse_position().x
 		
-	position.x = clamp(position.x, 50, rect_size.x - 30)
+	position.x = clamp(position.x, 65, rect_size.x - 40)
 	
 # When the ball hits the paddle.
 func _on_Paddle_body_entered(body):
@@ -19,9 +19,11 @@ func _on_Paddle_body_entered(body):
 		emit_signal("hit") # Calls _on_Paddle_hit in Main
 		
 		# Take the linear velocity vector of the ball, reflectsx it (goes in opposite direction),
-		# rotate it randomly, and multiply the Y coordinate by small number.
-		# This increases the Y velocity slightly every hit.
-		body.linear_velocity = body.linear_velocity.reflect(Vector2(-1, 0)).rotated(rand_range(-0.35, 0.35)) * 1.05
+		# rotate it randomly, and multiply the vector by small number.
+		# This increases the velocity slightly every hit.
+		body.linear_velocity = body.linear_velocity.reflect(Vector2(-1, 0)).rotated(rand_range(-0.35, 0.35))
+		body.linear_velocity.y *= 1.04
+		print(body.linear_velocity)
 		var x = body.linear_velocity.x
 		var y = body.linear_velocity.y
 		if (abs(x) / abs(y)) > 1.1:
